44e76c835ae0c0e903cf97b3485bd30ed64f7c78
Lorena Barreto <lorenasimedo@gmail.com>
2017-10-08 18:35:15 -0300

    Código do site

--- jogoBreakout/breakout.h
+++ jogoBreakout/breakout.h
@@ -1,22 +1,48 @@
 #ifndef BREAKOUT_H
 #define BREAKOUT_H
 
-#include <QMainWindow>
+#pragma once
 
-namespace Ui {
-class breakout;
-}
+#include <QWidget>
+#include <QKeyEvent>
+#include "ball.h"
+#include "brick.h"
+#include "paddle.h"
 
-class breakout : public QMainWindow
-{
-    Q_OBJECT
+class Breakout : public QWidget {
 
-public:
-    explicit breakout(QWidget *parent = 0);
-    ~breakout();
+  public:
+    Breakout(QWidget *parent = 0);
+    ~Breakout();
 
-private:
-    Ui::breakout *ui;
+  protected:
+    void paintEvent(QPaintEvent *);
+    void timerEvent(QTimerEvent *);
+    void keyPressEvent(QKeyEvent *);
+    void keyReleaseEvent(QKeyEvent *);
+    void drawObjects(QPainter *);
+    void finishGame(QPainter *, QString);
+    void moveObjects();
+
+    void startGame();
+    void pauseGame();
+    void stopGame();
+    void victory();
+    void checkCollision();
+
+  private:
+    int x;
+    int timerId;
+    static const int N_OF_BRICKS = 30;
+    static const int DELAY = 10;
+    static const int BOTTOM_EDGE = 400;
+    Ball *ball;
+    Paddle *paddle;
+    Brick *bricks[N_OF_BRICKS];
+    bool gameOver;
+    bool gameWon;
+    bool gameStarted;
+    bool paused;
 };
 
 #endif // BREAKOUT_H
